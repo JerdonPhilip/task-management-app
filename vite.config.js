2779393722a79c3915bfa4d6f7cb439d8b573a05
Jerdon Philip G. Macaraeg <jerdonphilipmacaraeg@gmail.com>
2025-10-14 22:59:51 +0800

    added deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()]
+    plugins: [vue()],
+    base: "/task-management-app/" // Add this line - use your repo name
 });
